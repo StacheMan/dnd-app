0827707984cfa0af4f7561d061814cf8d5c596c4
Matt Baltzer <baltzer.matt@gmail.com>
2022-08-12 18:41:31 -0700

    Added background images, logo in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import "./dice";
 import Layout from "./layout";
+import Logo from "./assests/updatedlogo.png";
+import Rolls from "./assests/d20.png";
 
 const styles = require("./index.css");
 
 function App() {
   return (
     <Layout>
-      <link rel="stylesheet" type="text/css" href="./index.css" />
       <div>
-        <h1>Basic Character Ability Scores</h1>
+        <header>
+          <img className={styles.titleImage} alt="Logo" src={Logo}></img>
+          <h1>Basic Character Ability Scores</h1>
+        </header>
       </div>
-      <div className={styles.list}>
-        <p id="guts" />
-        <button id="button"> Roll </button>
-        <script src="./dice.ts" />
-        <script src="./index.css" />
-      </div>
+      <footer>
+        <div className={styles.list}>
+          <p id="guts" />
+          <button id="button"> Roll </button>
+          <script src="./dice.ts" />
+          <script src="./index.css" />
+        </div>
+      </footer>
     </Layout>
   );
 }
