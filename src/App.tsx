1e6e9f05db3fe1d6851375c35af1e6f227976eb5
Matt Baltzer <baltzer.matt@gmail.com>
2022-08-12 17:05:53 -0700

    Created new Layout, updated background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import "./App.css";
-import "./index.css";
 import "./dice";
-import * as styles from "./index.css";
+import Layout from "./layout";
+
+const styles = require("./index.css");
 
 function App() {
   return (
-    <body>
+    <Layout>
       <link rel="stylesheet" type="text/css" href="./index.css" />
       <div>
-        <h1>DND Starting Character Rolls</h1>
+        <h1>Basic Character Ability Scores</h1>
+      </div>
+      <div className={styles.list}>
         <p id="guts" />
         <button id="button"> Roll </button>
         <script src="./dice.ts" />
         <script src="./index.css" />
       </div>
-    </body>
+    </Layout>
   );
 }
 
