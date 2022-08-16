262841ae0f7ad98b3b778de89146f189d71d22d4
Matt Baltzer <baltzer.matt@gmail.com>
2022-08-15 17:32:12 -0700

    Created race selector section

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./dice";
 import Layout from "./layout";
 import Logo from "./assests/updatedlogo.png";
+import { Dropdown, Option } from "./Dropdown";
 // import Rolls from "./assests/d20.png";
 // import * as styles from "./index.css";
 
 const styles = require("./index.css");
 
 function App() {
+  const [optionValue, setOptionValue] = useState("");
+  const handleSelect = (x: any) => {
+    console.log(x.target.value);
+    setOptionValue(x.target.value);
+  };
   return (
     <Layout>
       <header>
-        <div className={styles.titleImage}>
+        <div>
           <img alt="Logo" src={Logo} height={150} width={250} />
         </div>
         <div>
           <h1>Basic Character Ability Scores</h1>
         </div>
       </header>
-      <div>{/* This area is for the races choice list */}</div>
+      <section>
+        <Dropdown formLabel="Choose a race" action="/" onChange={handleSelect}>
+          <Option />
+          <option value="Dwarf">Dwarf</option>
+          <option value="Elf">Elf</option>
+          <option value="Halfling">Halfling</option>
+          <option value="Human">Human</option>
+          <option value="Dragonborn">Dragonborn</option>
+          <option value="Gnome">Gnome</option>
+          <option value="Half Elf">Half Elf</option>
+          <option value="Half Orc">Half Orc</option>
+          <option value="Tiefling">Tiefling</option>
+        </Dropdown>
+        <p> You've selected {optionValue}!</p>
+      </section>
       <footer>
         <div className={styles.list}>
           <p id="guts" />
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
+import styled from "styled-components";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +13,27 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+export const DropdownWrapper = styled.form`
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
+`;
+
+export const StyledSelect = styled.select`
+  max-width: 20%;
+  height: 100%;
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+`;
+
+export const StyledOption = styled.option`
+  color: ${(props: any) => (props.selected ? "lightgrey" : "black")};
+`;
+
+export const StyledLabel = styled.label`
+  margin-bottom: 1rem;
+`;
 
 //export default Index;
 // reportWebVitals();
