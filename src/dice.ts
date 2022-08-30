cc99ba8c535a71fe6371ffe9541dd2f4b536beaf
Matt Baltzer <baltzer.matt@gmail.com>
2022-08-30 14:10:19 -0700

    Removed everything, updated to just dice

--- src/dice.ts
+++ src/dice.ts
@@ -1,27 +1,22 @@
-// import { Dice } from "dice-typescript";
 
-// const dice = new Dice ();
-// const result = dice.roll("1d20").total;
-// console.log(result);
+const button = document.getElementById("button") as HTMLButtonElement || {};
 
-// const button = document.getElementById("button") as HTMLButtonElement;
+const dice = {
+  sides: 18,
+  roll: function () {
+    const randomNumber = Math.floor(Math.random() * this.sides) + 3;
+    return randomNumber;
+  }
+};
 
-// const dice = {
-//   sides: 18,
-//   roll: function () {
-//     const randomNumber = Math.floor(Math.random() * this.sides) + 3;
-//     return randomNumber;
-//   }
-// };
+function printNumber(number:any) {
+  const guts = document.getElementById("guts") as HTMLElement;
+  guts.innerHTML = number;
+};
 
-// function printNumber(number:any) {
-//   const guts = document.getElementById("guts") as HTMLElement;
-//   guts.innerHTML = number;
-// };
-
-// button.onclick = function () {
-//   const result = dice.roll();
-//   printNumber(result);
-// };
+button.onclick = function () {
+  const result = dice.roll();
+  printNumber(result);
+};
 
 export {};
